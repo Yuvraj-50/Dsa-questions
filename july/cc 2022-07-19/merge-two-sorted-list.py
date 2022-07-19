fc8bb25d568e65e3535d8bca7d29cdb925b421c3
yuvraj singh <yuvrajsingh2348q@gmail.com>
2022-07-19 12:30:09 +0530

    merge two sorted list

---
+++ july/cc 2022-07-19/merge-two-sorted-list.py
@@ -0,0 +1,47 @@
+# question url :: https://leetcode.com/problems/merge-two-sorted-lists/submissions/
+
+# question instruction :::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+# You are given the heads of two sorted linked lists list1 and list2.
+
+# Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+# Return the head of the merged linked list.
+
+# example s;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;s;;;;;;;;;;;;;;
+
+# Input: list1 = [1,2,4], list2 = [1,3,4]
+# Output: [1,1,2,3,4,4]
+# Example 2:
+
+# Input: list1 = [], list2 = []
+# Output: []
+
+# solutioN :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from typing import Optional
+from xml.dom.minicompat import NodeList
+
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[NodeList], list2: Optional[NodeList]) -> Optional[NodeList]:
+        dummy  = NodeList()
+        temp = dummy
+        
+        while list1 and list2:
+            if list1.val < list2.val:
+                temp.next = list1
+                list1 = list1.next
+            else:
+                temp.next = list2
+                list2 = list2.next
+            temp = temp.next
+        if list1:
+            temp.next = list1
+        else:
+            temp.next = list2
+        return dummy.next
