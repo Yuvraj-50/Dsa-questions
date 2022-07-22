4ba047d477e7149a880b20a60ead0af6647b5129
yuvraj singh <yuvrajsingh2348q@gmail.com>
2022-07-22 17:42:12 +0530

    partition list

---
+++ july/cc 2022-07-22/partition-list.py
@@ -0,0 +1,41 @@
+# quesiton url :: https://leetcode.com/problems/partition-list/
+
+# question instruction ":::::::::::::::::::::::::::::::::::::::::::::::
+# 
+# Given the head of a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+# You should preserve the original relative order of the nodes in each of the two partitions.
+
+#  "
+
+# example s;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+# Input: head = [1,4,3,2,5,2], x = 3
+# Output: [1,2,2,4,3,5]
+# Example 2:
+
+# Input: head = [2,1], x = 2
+# Output: [1,2]
+
+# solution ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        l1 = left = ListNode(None)
+        r1 = right = ListNode(None)
+        
+        while head:
+            if head.val < x:
+                left.next = head
+                left = left.next
+            else:
+                right.next = head
+                right = right.next
+            head = head.next
+        left.next = r1.next
+        right.next = None
+        return l1.next
