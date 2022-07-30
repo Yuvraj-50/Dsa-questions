18ccc184788e675ae2e081c1bb8018ba7cb92d3d
yuvraj singh <yuvrajsingh2348q@gmail.com>
2022-07-30 20:17:03 +0530

    diamerete of a binary tree

---
+++ july/cc 2022-07-30/diameter-of-binary-tree.py
@@ -0,0 +1,48 @@
+# question url : https://leetcode.com/problems/diameter-of-binary-tree/submissions/
+
+# question instruction ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+# Given the root of a binary tree, return the length of the diameter of the tree.
+
+# The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+
+# The length of a path between two nodes is represented by the number of edges between them.
+
+ 
+
+# Example 1:
+
+
+# Input: root = [1,2,3,4,5]
+# Output: 3
+# Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
+# Example 2:
+
+# Input: root = [1,2]
+# Output: 1
+ 
+
+# Constraints:
+
+# The number of nodes in the tree is in the range [1, 104].
+# -100 <= Node.val <= 100
+
+# solution ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def solve(self, root):
+        if not root:
+            return 0, 0
+        ld, lp = self.solve(root.left)
+        rd, rp = self.solve(root.right)
+        
+        return 1+max(ld, rd) , max(lp , rp, ld+rd+1)
+    
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        return self.solve(root)[1]-1
